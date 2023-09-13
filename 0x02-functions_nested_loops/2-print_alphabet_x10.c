25d527813c70f061a89e3bdaf9489b0d11f8ba76
jkunle377 <jkunle377@gmail.com>
2023-09-12 17:26:08 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * void print_alphabet_x10(void) - prints 10 times the alphabet in lower case
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet_x10(void);
+{
+	int i, j;
+		for (i = 0; i <= 9; i++)
+		{
+			for (j = 97; j <= 122; j++)
+			{
+				_putchar(j);
+			}
+			_putchar('\n')
+		}
+}
+
