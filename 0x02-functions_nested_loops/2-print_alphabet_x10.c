b4015e9af46c9f835942db6cab45e7f498e1be7c
jkunle377 <jkunle377@gmail.com>
2023-09-12 17:45:05 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * void print_alphabet_x10(void) - prints 10 times the alphabet in lower case
+ * print_alphabet_x10 - prints 10 times the alphabet in lower case
  *
  * Return: Always 0
  */
