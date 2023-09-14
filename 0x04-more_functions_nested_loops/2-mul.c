1538aa259adf30b6eaa496f7f200e11a9bbba13d
jkunle377 <jkunle377@gmail.com>
2023-09-14 07:24:07 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - prints two integer
+ *
+ * @a: first number
+ * @b: second number
+ *
+ * Return: multiplication
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
