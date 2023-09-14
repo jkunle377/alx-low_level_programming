bfb40f9354e0cf1ecc604d2fb548468e3be5cdb8
jkunle377 <jkunle377@gmail.com>
2023-09-14 08:42:18 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print digits except 2 and 4
+ *
+ * Return 0
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		if (i != 50 && i != 52)
+		{
+		_putchar(i);
+		}
+	}
+_putchar('\n');
+}
