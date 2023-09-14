5af80916782ea46f4ef3a6eb8b76a1a5a83d75bd
jkunle377 <jkunle377@gmail.com>
2023-09-14 09:59:14 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -3,6 +3,8 @@
 /**
  * print_line - prints - character
  *
+ *@n: time straight line is printed
+ *
  * Return: 0
  */
 
@@ -15,7 +17,8 @@ void print_line(int n)
 		_putchar('\n');
 	}
 	else
-	{ for (i = 0; i < n; i++)
+	{
+	       	for (i = 0; i < n; i++)
 		{
 			_putchar('_');
 		}
