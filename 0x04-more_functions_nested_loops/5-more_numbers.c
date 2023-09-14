cfa24e7f65aa7feb556db9919e1429acb3052d7b
jkunle377 <jkunle377@gmail.com>
2023-09-14 09:17:00 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the number
+ *
+ * Return: 0
+ */
+
+void more_numbers(void)
+{
+		int i, ch;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (ch = 0; ch < 15; ch++)
+		{
+			if (ch >= 10)
+				_putchar((ch / 10) + 48);
+			_putchar((ch % 10) + 48);
+		}
+		_putchar('\n');
+	}
+}
